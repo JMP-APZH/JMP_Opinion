5bf9706b8c35be03eb284413f9d52539f88572d2
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-12-28 19:48:45 +0100

    add auth- and rootlayouts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './globals.css'
 import { Routes, Route } from 'react-router-dom'
 import SigninForm from './_auth/forms/SigninForm'
 import { Home } from './_root/pages'
+import SignupForm from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 
 const App = () => {
@@ -10,11 +13,15 @@ const App = () => {
 
         <Routes>
             {/* {public routes} */}
-            <Route path='/sign-in' element={<SigninForm />} />
-
+            <Route element={<AuthLayout />}>
+                <Route path='/sign-up' element={<SignupForm />} />
+                <Route path='/sign-in' element={<SigninForm />} />
+            </Route>
 
             {/* {private routes} */}
-            <Route index element={<Home />} />
+            <Route element={<RootLayout />}>
+                <Route index element={<Home />} />
+            </Route>
 
             {/* <div>
                 
