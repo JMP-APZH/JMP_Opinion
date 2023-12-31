edd81df10adbfc565cf11a95803034d665cb580f
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-12-31 18:20:27 +0100

    prepare for shadcn config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './globals.css'
 import { Routes, Route } from 'react-router-dom'
-import SigninForm from './_auth/forms/SigninForm'
-import { Home } from './_root/pages'
-import SignupForm from './_auth/forms/SignupForm'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import { Home } from './_root/pages'
+
+import './globals.css'
 
 
 const App = () => {
