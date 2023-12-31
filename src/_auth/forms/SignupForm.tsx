152add99e6d079fc239b326a56f32144c68ed655
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-12-31 18:38:45 +0100

    AuthLayout prep

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button"
 
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <div>
+      SignupForm
+
+      <div>
+        <Button>Click me</Button>
+      </div>
+
+    </div>
   )
 }
 
