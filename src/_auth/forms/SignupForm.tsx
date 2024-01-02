23c9456f54b62fe62d05ccdeaf08853ccef587c5
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-01-02 09:40:16 +0100

    signup form - adj globals.css to match tutorial outcome

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -48,26 +48,104 @@ const SignupForm = () => {
               src='/assets/images/logo.svg'
               alt='logo'
             />
+            <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+              Create a new account
+            </h2>
+            <p className="text-light-3 small-medium md:base-regular">
+              To use Opinion enter your details
+            </p>
+            <form 
+              onSubmit={form.handleSubmit(onSubmit)} 
+              className="flex flex-col gap-5 w-full mt-4"
+            >
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="pb-2">Name</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type='text' 
+                        className="shad-input" {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}     
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="pb-2">Username</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type='text' 
+                        className="shad-input" {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="pb-2">Email</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type='email' 
+                        className="shad-input" {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="pb-2">Password</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type='password' 
+                        className="shad-input" {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button 
+                type="submit"
+                className="flex items-center justify-center shad-button_primary"
+              >
+                Submit
+              </Button>
+            </form>
           </div>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
         </Form>
   )
 }
