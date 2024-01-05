f9df268f00e33e8eb0123b185d576a11d064fc5f
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-01-05 15:00:00 +0100

    AuthContext completed

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
+import { getCurrentUser } from '@/lib/appwrite/api';
 import { IcontextType } from '@/types';
 import { createContext, useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const INITIAL_USER = {
     id: '',
@@ -21,14 +23,83 @@ const INITIAL_STATE = {
 
 const AuthContext = createContext<IcontextType>(INITIAL_STATE)
 
-const AuthContext = () => {
+const AuthProvider = ({ children} : { children: React.ReactNode }) => {
+    const [user, setUser] = useState<IUser>(INITIAL_USER);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const navigate = useNavigate();
+
+    const checkAuthUser = async () => {
+        try {
+            const currentAccount = await getCurrentUser();
+
+            if(currentAccount) {
+                setUser({
+                    id: currentAccount.$id,
+                    name: currentAccount.name,
+                    username: currentAccount.username,
+                    imageUrl: currentAccount.imageUrl,
+                    bio: currentAccount.bio,
+                })
+            
+                setIsAuthenticated(true);
+    
+                return true
+            }
+
+            return false
+
+            // ==> Destructured version
+            // const { $id, name, username, email, imageUrl, bio, } = await getCurrentUser();
+
+            // if(currentAccount) {
+            //     setUser({
+            //         id: $id,
+            //         name,
+            //         username,
+            //         imageUrl,
+            //         bio,
+            //     })}
+
+                
+        } catch (error) {
+            console.log(error)
+            return false;
+        } finally {
+           setIsLoading(false)     
+        }
+    };
+
+    // We have to call this fonction everytime we reload the page therefroe:
+    useEffect(() => {
+      if(
+        localStorage.getItem('cookieFallback') === '[]' || localStorage.getItem('cookieFallback') === null
+      ) navigate('/sign-in')
+
+      checkAuthUser()
+    }, []);
+    
+
+    const value = {
+        user,
+        setUser,
+        isLoading,
+        isAuthenticated,
+        setIsAuthenticated,
+        checkAuthUser,
+    }
+
   return (
-    <div>AuthContext</div>
+    <AuthContext.Provider value={value}>
+        { children }
+    </AuthContext.Provider>
   )
 }
 
-export default AuthContext
+export default AuthProvider
 
+export const useUserContext = () => useContext(AuthContext);
 
 // +41 76 759 39 75 - peter
 // +41 51 282 15 97 - matze
