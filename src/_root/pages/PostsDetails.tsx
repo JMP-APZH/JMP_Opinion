2c7904821db3a6c149e9a9d764ed7756f1932c9a
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-01-22 15:19:03 +0100

    post details - design prep

--- src/_root/pages/PostsDetails.tsx
+++ src/_root/pages/PostsDetails.tsx
@@ -1,5 +1,6 @@
 import Loader from "@/components/shared/Loader";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { multiFormatDateString } from "@/lib/utils";
 import { Link, useParams } from "react-router-dom";
 
 const PostsDetails = () => {
@@ -11,28 +12,36 @@ const PostsDetails = () => {
     <div className="post_details-container">
       { isPending ? <Loader /> : (
         <div className="post_details-card">
-          <div className="flex items-center gap-3">
-                <Link to={`/profile/${post?.userId}`}>
-                    <img 
-                        src={ post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg' }
-                        alt='creator'
-                        className="rounded-full w-12 lg:h-12"
-                    />
-                </Link>
+          <img 
+            src={post?.imageUrl}
+            alt='creator'
+            className="post_details-img"
+          />
+          <div className="post_details-info">
+            <div className="flex-between w-full">            
+              <Link to={`/profile/${post?.userId}`}>
+                  <img 
+                      src={ post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg' }
+                      alt='creator'
+                      className="rounded-full w-12 lg:h-12"
+                  />
+              
                 <div className="flex flex-col">
                     <p className="base-medium lg:body-bold text-light-1">
-                        { post?.creator.name }
+                      { post?.creator.name }
                     </p>
                     <div className="flex-center gap-2 text-light-3">
                         <p className="subtle-semibold lg:small-regular">  
-                            {multiFormatDateString(post?.$createdAt)} 
+                          {multiFormatDateString(post?.$createdAt)} 
                         </p>
                         <p className="subtle-semibold lg:small-regular"  > 
-                            {post?.location} 
+                          {post?.location} 
                         </p>
                     </div>
                 </div>
+              </Link>
             </div>
+          </div>
         </div>
       )}
     </div>
