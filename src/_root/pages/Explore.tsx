b22ac61f82ff6b2f17047b8c1d3154b881b32567
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-01-22 19:17:05 +0100

    Explore Page - popular today - filter design

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -32,6 +32,15 @@ const Explore = () => {
         <h3 className="body-bold md:h3-bold w-full">
           Popular Today
         </h3>
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+          <p className="small-medium md:base-medium text-light-2">All</p>
+          <img 
+            src="/assets/icons/filter.svg" 
+            alt="filter" 
+            width={20}
+            height={20}
+            className="" />
+        </div>
       </div>
     </div>
   )
